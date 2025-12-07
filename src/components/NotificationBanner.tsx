a2467723b67f430ddda5cb68791e7c902814e184
김승주 <kimseungju@gimseungjuui-MacBookPro.local>
2025-12-07 20:22:25 +0900

    add: 알림 배너의 x 버튼 클릭 시 닫히도록 구현

--- src/components/NotificationBanner.tsx
+++ src/components/NotificationBanner.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 // Notification Banner Component
 const NotificationBanner: React.FC = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleClose = () => {
+        setIsVisible(false);
+    };
+
+    if (!isVisible) {
+        return null;
+    }
+
     return (
         <div className="notification-banner">
             <img src={process.env.PUBLIC_URL + '/Announce Megaphone 1.svg'} alt="Notification" className="notification-icon" />
@@ -10,7 +20,7 @@ const NotificationBanner: React.FC = () => {
                 음총팀 카카오톡 채널을 추가하고 빠르게 소식을 받아보세요!
             </div>
             <a href="http://pf.kakao.com/_zfixgn" className="notification-link">바로가기</a>
-            <button className="close-button">
+            <button className="close-button" onClick={handleClose}>
                 <img src={process.env.PUBLIC_URL + '/Close Icon (1) 1.svg'} alt="Close" className="close-icon" />
             </button>
         </div>
