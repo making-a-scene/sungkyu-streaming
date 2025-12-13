d25656c11f359cc3ecc98d2d643580b2c1c7bc1b
김승주 <kimseungju@gimseungjuui-MacBookPro.local>
2025-12-13 17:41:06 +0900

    add: 비활성 상단 메뉴의 경우 호버 시 준비중 메시지 표시

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,21 +23,25 @@ const Header: React.FC<HeaderProps> = ({ onNavigate }) => {
                     <img src={process.env.PUBLIC_URL + '홈.svg'} alt="Home" className="header-menu-icon"/>
                     <span className="header-menu-text">홈</span>
                 </div>
-                <div className="header-menu-item">
+                <div className="header-menu-item inactive">
                     <img src={process.env.PUBLIC_URL + '가이드.svg'} alt="Guide" className="header-menu-icon"/>
                     <span className="header-menu-text">가이드</span>
+                    <span className="tooltip-text">준비중</span>
                 </div>
-                <div className="header-menu-item">
+                <div className="header-menu-item inactive">
                     <img src={process.env.PUBLIC_URL + '스케줄.svg'} alt="Schedule" className="header-menu-icon"/>
                     <span className="header-menu-text">스케줄</span>
+                    <span className="tooltip-text">준비중</span>
                 </div>
-                <div className="header-menu-item">
+                <div className="header-menu-item inactive">
                     <img src={process.env.PUBLIC_URL + '차트.svg'} alt="Chart" className="header-menu-icon"/>
                     <span className="header-menu-text">차트</span>
+                    <span className="tooltip-text">준비중</span>
                 </div>
-                <div className="header-menu-item">
+                <div className="header-menu-item inactive">
                     <img src={process.env.PUBLIC_URL + '지원하기.svg'} alt="Apply" className="header-menu-icon"/>
                     <span className="header-menu-text">지원하기</span>
+                    <span className="tooltip-text">준비중</span>
                 </div>
             </div>
             <div className="social-icons">
