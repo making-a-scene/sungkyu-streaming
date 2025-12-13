b45d18137903c525ab6b0dbd39d309ce0638eef0
김승주 <kimseungju@gimseungjuui-MacBookPro.local>
2025-12-13 18:41:25 +0900

    add: 이메일 복사 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 interface HeaderProps {
     onNavigate?: (page: string) => void;
@@ -10,6 +11,15 @@ const Header: React.FC<HeaderProps> = ({ onNavigate }) => {
     const handleLogoClick = () => {
         if (onNavigate) {
             onNavigate('home');
+        }
+    };
+
+    const copyEmail = async () => {
+        try {
+            await navigator.clipboard.writeText("sungkyustream@gmail.com");
+            toast.success("이메일 주소가 클립보드에 복사되었습니다!");
+        } catch (e) {
+            toast.error("이메일 주소 복사에 실패했습니다. 다시 시도해주세요.");
         }
     };
 
@@ -45,9 +55,9 @@ const Header: React.FC<HeaderProps> = ({ onNavigate }) => {
                 </div>
             </div>
             <div className="social-icons">
-                <a href="http://pf.kakao.com/_zfixgn" className="header-social-icon">
-                    <img src={process.env.PUBLIC_URL + 'email.svg'} alt="Email" className="header-social-icon header-social-icon-email" />
-                </a>
+                <img src={process.env.PUBLIC_URL + 'email.svg'} alt="Email" className="header-social-icon header-social-icon-email"
+                    onClick={(e) => copyEmail()}/>
+                    <ToastContainer />
                 <a href="https://x.com/sungkyustream" className="header-social-icon">
                     <img src={process.env.PUBLIC_URL + 'xlogo.svg'} alt="X" className="header-social-icon header-social-icon-x" />
                 </a>
