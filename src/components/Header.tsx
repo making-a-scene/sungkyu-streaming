6b0bf011bd5ad95d5e4c2a296ad0a4981d6d4d64
김승주 <kimseungju@gimseungjuui-MacBookPro.local>
2025-12-13 17:27:42 +0900

    add: 모바일 화면에서는 헤더 메뉴의 아이콘이 사라지도록 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,15 +15,30 @@ const Header: React.FC<HeaderProps> = ({ onNavigate }) => {
 
     return (
         <header className="header">
-            <div className="logo" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
+            <div className="logo" onClick={handleLogoClick}>
                 <img src={process.env.PUBLIC_URL + 'logo.svg'} alt="Logo" className="logo-icon" />
             </div>
-            <div className="header-menu" style={{ cursor: 'pointer' }}>
-                <img src={process.env.PUBLIC_URL + '홈.svg'} alt="Home" className="header-menu-icon" />
-                <img src={process.env.PUBLIC_URL + '가이드.svg'} alt="Guide" className="header-menu-icon" />
-                <img src={process.env.PUBLIC_URL + '스케줄.svg'} alt="Schedule" className="header-menu-icon" />
-                <img src={process.env.PUBLIC_URL + '차트.svg'} alt="Chart" className="header-menu-icon" />
-                <img src={process.env.PUBLIC_URL + '지원하기.svg'} alt="Chart" className="header-menu-icon" />
+            <div className="header-menu">
+                <div className="header-menu-item active">
+                    <img src={process.env.PUBLIC_URL + '홈.svg'} alt="Home" className="header-menu-icon"/>
+                    <span className="header-menu-text">홈</span>
+                </div>
+                <div className="header-menu-item">
+                    <img src={process.env.PUBLIC_URL + '가이드.svg'} alt="Guide" className="header-menu-icon"/>
+                    <span className="header-menu-text">가이드</span>
+                </div>
+                <div className="header-menu-item">
+                    <img src={process.env.PUBLIC_URL + '스케줄.svg'} alt="Schedule" className="header-menu-icon"/>
+                    <span className="header-menu-text">스케줄</span>
+                </div>
+                <div className="header-menu-item">
+                    <img src={process.env.PUBLIC_URL + '차트.svg'} alt="Chart" className="header-menu-icon"/>
+                    <span className="header-menu-text">차트</span>
+                </div>
+                <div className="header-menu-item">
+                    <img src={process.env.PUBLIC_URL + '지원하기.svg'} alt="Apply" className="header-menu-icon"/>
+                    <span className="header-menu-text">지원하기</span>
+                </div>
             </div>
             <div className="social-icons">
                 <a href="http://pf.kakao.com/_zfixgn" className="header-social-icon">
