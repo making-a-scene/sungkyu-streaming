de553e1234e65c9a746a949459e568dad87fcdc6
김승주 <kimseungju@gimseungjuui-MacBookPro.local>
2025-12-13 21:06:37 +0900

    fix: 음악방송 가이드 탭 변경

--- src/pages/MusicBroadcast.tsx
+++ src/pages/MusicBroadcast.tsx
@@ -11,7 +11,7 @@ interface MusicBroadcastProps {
 }
 
 const MusicBroadcast: React.FC<MusicBroadcastProps> = ({ onNavigate }) => {
-    const tabs = ['차트집계', '스케줄', '재화·투표권 모으기', '음악중심', '뮤직뱅크', '인기가요', '쇼챔피언', '엠카운트다운'];
+    const tabs = ['차트집계', '스케줄', '쇼챔피언', '엠카운트다운', '뮤직뱅크', '음악중심', '인기가요'];
 
     return (
         <div className="app">
@@ -19,7 +19,7 @@ const MusicBroadcast: React.FC<MusicBroadcastProps> = ({ onNavigate }) => {
             <main className="main-content">
                 {/*<NotificationBanner />*/}
                 <div className="page-title-text" style={{ marginBottom: '24px' }}>음악방송 가이드</div>
-                <TabView tabs={tabs} defaultTab={2} />
+                <TabView tabs={tabs} defaultTab={0} />
                 <img src={process.env.PUBLIC_URL + '/재화투표권모으기가이드.svg'} alt="guide" style={{ width: '880px', display: 'block' }} />
             </main>
             <Footer />
