04cad11a9e16bb46a5d0c9b6ef66e6dd12cef1b7
김승주 <kimseungju@gimseungjuui-MacBookPro.local>
2025-12-19 23:59:21 +0900

    add: 아이디 생성 가이드 페이지에 이미지 추가

--- src/pages/guide/IdGeneration.tsx
+++ src/pages/guide/IdGeneration.tsx
@@ -16,7 +16,30 @@ const IdGeneration = () => {
             <GuideMenu />
             <TabView tabs={tabs} defaultTab={0} onTabChange={setSelectedTab} />
             <main className="main-content">
-
+                {selectedTab === 0 && (
+                    <img src={process.env.PUBLIC_URL + '/guide-melon.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
+                )}
+                {selectedTab === 1 && (
+                    <img src={process.env.PUBLIC_URL + '/guide-genie.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
+                )}
+                {selectedTab === 2 && (
+                    <><img src={process.env.PUBLIC_URL + '/guide-bugs.png'} alt="guide"
+                           style={{width: '100%', maxWidth: '880px', display: 'block'}}/><img
+                        src={process.env.PUBLIC_URL + '/guide-number.png'} alt="guide"
+                        style={{width: '100%', maxWidth: '880px', display: 'block'}}/></>
+                )}
+                {selectedTab === 3 && (
+                    <img src={process.env.PUBLIC_URL + '/guide-flo.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
+                )}
+                {selectedTab === 4 && (
+                    <img src={process.env.PUBLIC_URL + '/guide-vibe.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
+                )}
+                {selectedTab === 5 && (
+                    <img src={process.env.PUBLIC_URL + '/guide-number.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
+                )}
+                {selectedTab === 6 && (
+                    <img src={process.env.PUBLIC_URL + '/guide-higher.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
+                )}
             </main>
             <Footer />
         </div>
