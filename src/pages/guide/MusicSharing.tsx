ad1045374379c9d58da7ad68ad1d1d768aba790c
김승주 <saram@aipia.co.kr>
2026-01-01 19:17:46 +0900

    add: 음악나누기 가이드 서브메뉴 미적용 문제 해결

--- src/pages/guide/MusicSharing.tsx
+++ src/pages/guide/MusicSharing.tsx
@@ -3,14 +3,27 @@ import '../../App.css';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import GuideMenu from '../../components/GuideMenu';
+import SubMenu from '../../components/SubMenu';
 
 const IdGeneration = () => {
+
+    const guideMenus =  [
+        { label: '아이디', path: '/guide/id-generation', disabled: false },
+        { label: '스트리밍', path: '/guide/streaming', disabled: true },
+        { label: '다운로드', path: '/guide/download', disabled: true },
+        { label: '음악방송', path: '/guide/music-broadcast' },
+        { label: '투표', path: '/guide/vote' },
+        { label: '선물하기', path: '/guide/gift', disabled: true },
+        { label: 'MV', path: '/guide/mv', disabled: true },
+        { label: '숏폼·SNS', path: '/guide/shorts-sns', disabled: true },
+        { label: '라디오', path: '/guide/radio', disabled: true },
+        { label: '컬러링', path: '/guide/coloring', disabled: true }
+    ];
 
     return (
         <div className="app">
             <Header />
-            <GuideMenu />
+            <SubMenu menuItems={guideMenus} />
             <main className="main-content">
                 <img src={process.env.PUBLIC_URL + '/guide-genie-music-sharing.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
             </main>
