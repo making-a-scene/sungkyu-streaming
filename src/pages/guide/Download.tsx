840f3cc4fe998dd73707c6a1b373dd7b4341e932
making-a-scene <4936joo@naver.com>
2026-02-21 12:25:22 +0900

    fix: mv 다운로드 가이드를 별도 탭으로 분리

--- src/pages/guide/Download.tsx
+++ src/pages/guide/Download.tsx
@@ -5,7 +5,7 @@ import Footer from "../../components/Footer";
 import TabView from "../../components/TabView";
 
 const Download = () => {
-    const tabs = ['멜론', '지니', '벅스', '카카오뮤직'];
+    const tabs = ['멜론', '지니', '벅스', '카카오뮤직', '멜론 MV 다운로드', '벅스 MV 다운로드'];
     const [selectedTab, setSelectedTab] = useState(0);
 
     return (
@@ -17,8 +17,6 @@ const Download = () => {
                 {selectedTab === 0 && (
                     <><img src={process.env.PUBLIC_URL + '/melon-download-guide.png'} alt="guide"
                            style={{width: '100%', maxWidth: '880px', display: 'block'}}/><img
-                        src={process.env.PUBLIC_URL + '/melon-mv-download-guide.png'} alt="guide"
-                        style={{width: '100%', maxWidth: '880px', display: 'block'}}/><img
                         src={process.env.PUBLIC_URL + '/melon-cash-charge-guide.png'} alt="guide"
                         style={{width: '100%', maxWidth: '880px', display: 'block'}}/></>
                 )}
@@ -26,13 +24,16 @@ const Download = () => {
                     <img src={process.env.PUBLIC_URL + '/genie-download-guide.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
                 )}
                 {selectedTab === 2 && (
-                    <><img src={process.env.PUBLIC_URL + '/bugs-music-download-guide.png'} alt="guide"
-                           style={{width: '100%', maxWidth: '880px', display: 'block'}}/><img
-                        src={process.env.PUBLIC_URL + '/bugs-mv-download-guide.png'} alt="guide"
-                        style={{width: '100%', maxWidth: '880px', display: 'block'}}/></>
+                    <img src={process.env.PUBLIC_URL + '/bugs-music-download-guide.png'} alt="guide" style={{width: '100%', maxWidth: '880px', display: 'block'}}/>
                 )}
                 {selectedTab === 3 && (
                     <img src={process.env.PUBLIC_URL + '/kakaomusic-download-guide.png'} alt="guide" style={{ width: '100%', maxWidth: '880px', display: 'block' }} />
+                )}
+                {selectedTab === 4 && (
+                    <img src={process.env.PUBLIC_URL + '/melon-mv-download-guide.png'} alt="guide" style={{width: '100%', maxWidth: '880px', display: 'block'}}/>
+                )}
+                {selectedTab === 5 && (
+                    <img src={process.env.PUBLIC_URL + '/bugs-mv-download-guide.png'} alt="guide" style={{width: '100%', maxWidth: '880px', display: 'block'}}/>
                 )}
             </main>
             <Footer />
