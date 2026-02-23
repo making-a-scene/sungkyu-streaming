204faedbef217d951e6c13e95e2ff8ad75bbc09d
making-a-scene <4936joo@naver.com>
2026-02-24 08:05:00 +0900

    fix: 환경변수명 변경

--- api/charts.ts
+++ api/charts.ts
@@ -2,8 +2,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Redis } from '@upstash/redis';
 
 const redis = new Redis({
-  url: process.env.KV_REST_API_URL!,
-  token: process.env.KV_REST_API_TOKEN!,
+  url: process.env.REDIS_URL!,
+  token: process.env.REST_API_TOKEN!,
 });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
