0e1da605d94ef45af6bdcf583cbf73c20e307992
making-a-scene <4936joo@naver.com>
2026-03-02 16:20:25 +0700

    chore: 스트리밍 카운터 교체

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
                 <div className="home-bottom-group">
                     <AnnouncementSection />
                     <StreamingPills />
-                    <StreamingCounter releaseKST={new Date('2026-02-21T18:00:00+09:00')} songName="Over It" />
+                    <StreamingCounter releaseKST={new Date('2026-03-02T18:00:00+09:00')} songName="널 떠올리면 (When I think about you)" />
                     <div className="home-guide-section">
                         <h2 className="home-section-title">음원 총공 방법이 궁금하다면!</h2>
                         <div className="home-guide-content">
