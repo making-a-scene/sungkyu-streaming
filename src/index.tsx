fc094d0ffc16af78fd444b960d6ccf729d0a9177
subin <110241993+marybin99@users.noreply.github.com>
2026-08-20 19:45:31 +0900

    feat: pwa 기능 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { registerServiceWorker } from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,3 +13,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+registerServiceWorker();
